09416494c1a5cf9944eac617b0ac3ba3ab51e324
伊東和成 <itokazunari5375@icloud.com>
2022-12-10 22:50:59 +0900

    feat: 色つけるやつ

--- src/extension.ts
+++ src/extension.ts
@@ -1,101 +1,41 @@
 import * as vscode from "vscode";
+
+const decorationType = vscode.window.createTextEditorDecorationType({
+  backgroundColor: "green",
+  border: "2px solid white",
+});
+
 export function activate(context: vscode.ExtensionContext) {
-  /* commands */
-  let cmd = vscode.commands.registerCommand("vscode-context.capitalize", () => {
-    customContext("vscode-context.capitalize");
+  vscode.workspace.onWillSaveTextDocument((event) => {
+    const openEditor = vscode.window.visibleTextEditors.filter(
+      (editor) => editor.document.uri === event.document.uri
+    )[0];
+    decorate(openEditor);
   });
-  let cmd2 = vscode.commands.registerCommand("vscode-context.upText", () => {
-    customContext("vscode-context.upText");
-  });
-  let cmd3 = vscode.commands.registerCommand("vscode-context.lowerCase", () => {
-    customContext("vscode-context.lowerCase");
-  });
-  let cmd4 = vscode.commands.registerCommand("vscode-context.lowerText", () => {
-    customContext("vscode-context.lowerText");
-  });
-  let cmd5 = vscode.commands.registerCommand("vscode-context.any-check", () => {
-    customContext("vscode-context.any-check");
-  });
-
-  context.subscriptions.push(cmd);
-  context.subscriptions.push(cmd2);
-  context.subscriptions.push(cmd3);
-  context.subscriptions.push(cmd4);
-  context.subscriptions.push(cmd5);
 }
 
-// おみくじ
-function getWebviewContent() {
-  return `<!DOCTYPE html>
-   <html lang="ja">
-   <head>
-       <meta charset="UTF-8">
-       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   </head>
-   <body>
-       <img src="https://www.mbs.jp/mbs-column/p-battle/thumb/20190924175210-dfacb10b6a288de4a8506b3c24a433dd3684b841.jpg" />
-   </body>
-   </html>`;
-}
+function decorate(editor: vscode.TextEditor) {
+  let sourceCode = editor.document.getText();
+  let regex = /(console\.log)/;
 
-const customContext = (key: string) => {
-  const editor = vscode.window.activeTextEditor;
-  if (editor) {
-    const document = editor.document;
-    const selection = editor.selection;
-    let text = document.getText(selection);
-    if (text) {
-      switch (key) {
-        case "vscode-context.any-check":
-          break;
-        case "vscode-context.capitalize":
-          const upText = text.charAt(0).toUpperCase() + text.slice(1);
-          editor.edit((edit) => {
-            edit.replace(selection, upText);
-          });
-          break;
-        /* anyチェックロジック用 */
-        case "vscode-context.upText":
-          let cur_selection = editor.selection; // 選択範囲取得
-          let doc = editor.document; // ドキュメント取得
-          let t = doc.getText(cur_selection); //取得されたテキスト
+  let decorationsArray: vscode.DecorationOptions[] = [];
 
-          const message = vscode.window.showInformationMessage(`${t}`, {
-            modal: true,
-          });
-          const panel = vscode.window.createWebviewPanel(
-            "omikuji",
-            `${t}`,
-            vscode.ViewColumn.Beside,
-            {}
-          );
+  const sourceCodeArr = sourceCode.split("\n");
 
-          panel.webview.html = getWebviewContent();
-        // const upText2 = text.toUpperCase();
-        // editor.edit((edit) => {
-        //   edit.replace(selection, upText2);
-        // });
-        // break;
+  for (let line = 0; line < sourceCodeArr.length; line++) {
+    let match = sourceCodeArr[line].match(regex);
 
-        case "vscode-context.lowerCase":
-          let lowerCase = text.toLowerCase();
-          lowerCase = lowerCase.charAt(0).toUpperCase() + lowerCase.slice(1);
-          editor.edit((edit) => {
-            edit.replace(selection, lowerCase);
-          });
-          break;
+    if (match !== null && match.index !== undefined) {
+      let range = new vscode.Range(
+        new vscode.Position(line, match.index),
+        new vscode.Position(line, match.index + match[1].length)
+      );
 
-        case "vscode-context.lowerText":
-          let lowerText = text.toLowerCase();
-          editor.edit((edit) => {
-            edit.replace(selection, lowerText);
-          });
-          break;
-      }
+      let decoration = { range };
+
+      decorationsArray.push(decoration);
     }
   }
-};
 
-export function deactivate() {}
-// 先頭意外を小文字
-// 小文字に
+  editor.setDecorations(decorationType, decorationsArray);
+}

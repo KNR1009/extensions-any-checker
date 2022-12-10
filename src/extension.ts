503127c72e07e68b2f9fbbbb3a1401a4fc28d710
伊東和成 <itokazunari5375@icloud.com>
2022-12-10 23:10:07 +0900

    feat: 不要部分の削除

--- src/extension.ts
+++ src/extension.ts
@@ -1,51 +1,11 @@
 import * as vscode from "vscode";
-import Decorator from "./decorator";
 
 export function activate(context: vscode.ExtensionContext) {
-  /* commands */
-  let cmd = vscode.commands.registerCommand("vscode-context.capitalize", () => {
-    customContext("vscode-context.capitalize");
-  });
-  let cmd2 = vscode.commands.registerCommand("vscode-context.upText", () => {
-    customContext("vscode-context.upText");
-  });
-  let cmd3 = vscode.commands.registerCommand("vscode-context.lowerCase", () => {
-    customContext("vscode-context.lowerCase");
-  });
-  let cmd4 = vscode.commands.registerCommand("vscode-context.lowerText", () => {
-    customContext("vscode-context.lowerText");
-  });
-  let cmd5 = vscode.commands.registerCommand("vscode-context.any-check", () => {
+  let cmd = vscode.commands.registerCommand("vscode-context.any-check", () => {
     customContext("vscode-context.any-check");
   });
-
-  // デコレーション
-  let decorator = new Decorator();
-
-  vscode.window.onDidChangeActiveTextEditor(
-    (editor) => {
-      decorator.changeActiveTextEditor(editor);
-    },
-    null,
-    context.subscriptions
-  );
-
-  vscode.workspace.onDidChangeTextDocument(
-    (event) => {
-      decorator.changeTextDocument(event);
-    },
-    null,
-    context.subscriptions
-  );
-
   context.subscriptions.push(cmd);
-  context.subscriptions.push(cmd2);
-  context.subscriptions.push(cmd3);
-  context.subscriptions.push(cmd4);
-  context.subscriptions.push(cmd5);
 }
-
-// style
 
 // 画像呼び出し
 function getWebviewContent() {
@@ -70,15 +30,6 @@ const customContext = (key: string) => {
     if (text) {
       switch (key) {
         case "vscode-context.any-check":
-          break;
-        case "vscode-context.capitalize":
-          const upText = text.charAt(0).toUpperCase() + text.slice(1);
-          editor.edit((edit) => {
-            edit.replace(selection, upText);
-          });
-          break;
-        /* anyチェックロジック用 */
-        case "vscode-context.upText":
           let cur_selection = editor.selection; // 選択範囲取得
           let doc = editor.document; // ドキュメント取得
           let t = doc.getText(cur_selection); //取得されたテキスト
@@ -95,31 +46,9 @@ const customContext = (key: string) => {
           );
 
           panel.webview.html = getWebviewContent();
-        // const upText2 = text.toUpperCase();
-        // editor.edit((edit) => {
-        //   edit.replace(selection, upText2);
-        // });
-        // break;
-
-        case "vscode-context.lowerCase":
-          let lowerCase = text.toLowerCase();
-          lowerCase = lowerCase.charAt(0).toUpperCase() + lowerCase.slice(1);
-          editor.edit((edit) => {
-            edit.replace(selection, lowerCase);
-          });
-          break;
-
-        case "vscode-context.lowerText":
-          let lowerText = text.toLowerCase();
-          editor.edit((edit) => {
-            edit.replace(selection, lowerText);
-          });
-          break;
       }
     }
   }
 };
 
 export function deactivate() {}
-// 先頭意外を小文字
-// 小文字に

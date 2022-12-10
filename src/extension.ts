cbf94c39a523a720119e9a987b6dbfb8907b90d2
伊東和成 <itokazunari5375@icloud.com>
2022-12-10 23:38:00 +0900

    feat: 完成(まだ直すべき箇所はある)

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,41 @@
 import * as vscode from "vscode";
 
+const decorationType = vscode.window.createTextEditorDecorationType({
+  textDecoration: "line-through;",
+  color: "red;",
+});
+
+// デコレーション用
+function decorate(editor: vscode.TextEditor) {
+  let sourceCode = editor.document.getText();
+  let regex = /(any)/;
+
+  let decorationsArray: vscode.DecorationOptions[] = [];
+
+  const sourceCodeArr = sourceCode.split("\n");
+
+  for (let line = 0; line < sourceCodeArr.length; line++) {
+    let match = sourceCodeArr[line].match(regex);
+
+    if (match !== null && match.index !== undefined) {
+      let range = new vscode.Range(
+        new vscode.Position(line, match.index),
+        new vscode.Position(line, match.index + match[1].length)
+      );
+
+      let decoration = { range };
+
+      decorationsArray.push(decoration);
+    }
+  }
+  editor.setDecorations(decorationType, decorationsArray);
+}
+
 export function activate(context: vscode.ExtensionContext) {
   let cmd = vscode.commands.registerCommand("vscode-context.any-check", () => {
     customContext("vscode-context.any-check");
   });
+
   context.subscriptions.push(cmd);
 }
 
@@ -30,13 +62,12 @@ const customContext = (key: string) => {
     if (text) {
       switch (key) {
         case "vscode-context.any-check":
-          let cur_selection = editor.selection; // 選択範囲取得
-          let doc = editor.document; // ドキュメント取得
-          let t = doc.getText(cur_selection); //取得されたテキスト
-
-          const message = vscode.window.showInformationMessage(`${t}`, {
-            modal: true,
-          });
+          const message = vscode.window.showInformationMessage(
+            `いい加減にしなさい`,
+            {
+              modal: true,
+            }
+          );
 
           const panel = vscode.window.createWebviewPanel(
             "omikuji",
@@ -46,6 +77,14 @@ const customContext = (key: string) => {
           );
 
           panel.webview.html = getWebviewContent();
+
+          // 色つける
+          vscode.workspace.onWillSaveTextDocument((event) => {
+            const openEditor = vscode.window.visibleTextEditors.filter(
+              (editor) => editor.document.uri === event.document.uri
+            )[0];
+            decorate(openEditor);
+          });
       }
     }
   }
